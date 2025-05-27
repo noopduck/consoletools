42d5389c412bf3cf6ccefcc32ad480a324b17350
noopduck <noopduck@dev.null>
2025-05-28 00:29:38 +0200

    Added nvchad to my system instead

---
+++ DOTFILES/.config/nvimchad/lua/options.lua
@@ -0,0 +1,28 @@
+require "nvchad.options"
+
+-- add yours here!
+local o = vim.opt
+-- o.cursorlineopt ='both' -- to enable cursorline!
+o.relativenumber = true
+o.clipboard = "unnamedplus"
+o.autoindent = true
+o.cursorline = true
+o.expandtab = true
+o.shiftwidth = 2
+o.tabstop = 2
+o.encoding = "UTF-8"
+o.ruler = true
+o.mouse = "a"
+o.title = true
+o.hidden = true
+o.ttimeoutlen = 0
+o.wildmenu = true
+o.showcmd = true
+o.showmatch = true
+o.inccommand = "split"
+o.splitright = true
+o.splitbelow = true
+o.termguicolors = true
+
+o.list = true
+o.listchars = { trail = "-", eol = "↲", tab = "» ", space = "·", nbsp = "+"}
