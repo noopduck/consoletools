6db37878a2dfa1fce159ffe76ac8b9855b4f686a
noopduck <noopduck@dev.null>
2025-09-22 20:04:58 +0200

    Made lazynvim opaque

--- DOTFILES/.config/nvimlazy/init.lua
+++ DOTFILES/.config/nvimlazy/init.lua
@@ -1,3 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.bubbles")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
