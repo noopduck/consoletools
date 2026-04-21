03eaef08750b11f5e129a19541dfa8dc3908afe0
Rahmi Acar <rahmiac@protonmail.com>
2026-04-21 23:50:00 +0200

    shell_cust../tmux.. cleaning out powerline

--- shell_customizations/tmux_setup.sh
+++ shell_customizations/tmux_setup.sh
@@ -15,13 +15,6 @@ if [[ -f /etc/debian_version ]]; then
     echo "apt install tmux if not already installed"
     sudo apt -y install tmux curl
 
-    # make sure that python is python3 and not anything else...
-    [[ ! $(ls /usr/bin/python|grep python3) ]] && {
-        echo "/usr/bin/python does not point at python3 as it should, fixing it."
-        sudo unlink /usr/bin/python
-        sudo ln -s /usr/bin/python3 /usr/bin/python
-    }
-
 elif [[ "$(uname -r)" =~ "MANJARO" ]]; then
         echo "pacman installing tmux if not already installed"
         [[ ! $(pacman -Qs tmux|grep "local/tmux") ]] && {
@@ -59,12 +52,8 @@ fi
     }    
 }
 
-# Setup powerline
-curl https://raw.githubusercontent.com/noopduck/consoletools/master/shell_customizations/powerline_setup.sh | bash
-
 TMUX_CONF_PART=$(curl https://raw.githubusercontent.com/noopduck/consoletools/master/DOTFILES/.tmux.conf)
 
-# Setup path for powerline temporarily
 export PATH=$PATH:~/.local/bin
 
 # Copy the .tmux.conf file into ~/
@@ -76,16 +65,11 @@ export PATH=$PATH:~/.local/bin
     echo "$TMUX_CONF_PART" > ~/.tmux.conf
 } 
 
-# Make sure that this line is executed after copying in the .tmux.conf file for debian
-[[ -f /etc/debian_version ]] && {
-    # Replace this line in debian
-    sed -i 's#source.*powerline.conf.*$#source /usr/share/powerline/bindings/tmux/powerline.conf#' ~/.tmux.conf
-}
-
 # Inside tmux run this to setup the plugins
 echo "INSIDE tmux RUN ~/.tmux/plugins/tpm/tpm to install tpm"
 echo "INSIDE tmux RUN CTRL+B followed by SHIFT+I"
 
 # Add mtmux script into /usr/local/bin
-sudo curl https://raw.githubusercontent.com/noopduck/consoletools/master/mtmux -o /usr/local/bin/mtmux
-sudo chmod +x /usr/local/bin/mtmux
+sudo curl https://raw.githubusercontent.com/noopduck/consoletools/master/mtmux -o ~/.local/bin/mtmux
+sudo chmod +x ~/.local/bin/mtmux
+
