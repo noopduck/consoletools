06082ecf8ac2c19b8bd9c8c75b8cb46c6d66c393
Rahmi Acar <rahmiac@protonmail.com>
2026-04-21 23:53:40 +0200

    Removed nvim of the past

--- DOTFILES/.config/nvim/lsp/capabilities.lua
+++
@@ -1,9 +0,0 @@
-vim.lsp.config("*", {
-	capabilities = {
-		textDocument = {
-			semanticTokens = {
-				multilineTokenSupport = true,
-			},
-		},
-	},
-})
--- DOTFILES/.config/nvim/lua/options.lua
+++
@@ -1,4 +0,0 @@
-vim.g.lazyvim_python_lsp = "pyright"
-vim.g.python3_host_prog = "/usr/bin/python3"
-
-require("nvim-treesitter.configs").setup({ highlight = { enable = true } })
